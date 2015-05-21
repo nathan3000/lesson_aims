a5bcd7306f4c9ecb2ab9e9ee8c0d954a0dabe693
Nathan Fisher <nooglefish@gmail.com>
2015-05-21 12:26:24 +0100

    Create db in __init__.py

--- __init__.py
+++ __init__.py
@@ -8,4 +8,9 @@ app.config.from_pyfile('config.cfg')
 
 db.init_app(app)
 
+with app.app_context():
+        # Extensions like Flask-SQLAlchemy now know what the "current" app
+        # is while within this block. Therefore, you can now run........
+        db.create_all()
+
 import controllers
