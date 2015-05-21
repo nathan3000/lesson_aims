5f61657cc000ee3c6d5dbcb3e372aa26b7e1ffa6
Nathan Fisher <nooglefish@gmail.com>
2015-05-21 14:04:48 +0100

    Import Group

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db
+from models import db, Group
 
 app = Flask(__name__)
 
@@ -9,10 +9,10 @@ app.config.from_pyfile('config.cfg')
 db.init_app(app)
 
 with app.app_context():
-	# Extensions like Flask-SQLAlchemy now know what the "current" app
-	# is while within this block. Therefore, you can now run........
-	db.create_all()
-	db.session.add(Group('Diggers', 'Preschool'))
+    # Extensions like Flask-SQLAlchemy now know what the "current" app
+    # is while within this block. Therefore, you can now run........
+    db.create_all()
+    db.session.add(Group('Diggers', 'Preschool'))
 	db.session.add(Group('Discoverers', 'Year R-2'))
 	db.session.add(Group('Explorers', 'Years 3-6'))
 	db.session.commit()
